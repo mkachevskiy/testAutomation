51faf6da6eafc6bd3a07130a60e3c51f038c4d86
mkachevskiy <mkachevskiy@gmail.com>
2024-06-04 22:28:48 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,18 +10,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.seleniumhq.selenium:selenium-java:4.11.0")
-    implementation("commons-io:commons-io:2.11.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.21.0")
+    implementation("commons-io:commons-io:2.16.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
-    testImplementation("io.qameta.allure:allure-junit5:2.23.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
+    testImplementation("io.qameta.allure:allure-junit5:2.27.0")
 
-    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.5")
+    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.13")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-    compileOnly("org.projectlombok:lombok:1.18.24")
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.getByName<Test>("test") {
